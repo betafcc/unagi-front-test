6cc5c58a460f4c10894e5efda9e025cb92aea11c
betafcc <betafcc@gmail.com>
2024-06-26 11:24:30 +0200

    refactor: create components directory

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -28,9 +28,7 @@ export const Card: FC<CardData & Omit<ComponentProps<'div'>, keyof CardData>> =
           alt={formatName(player)}
         />
         <h2 className="card__title">{formatName(player)}</h2>
-        <h3 className="card__subtitle">
-          {formatDate(new Date(), 'fr')}
-        </h3>
+        <h3 className="card__subtitle">{formatDate(new Date(), 'fr')}</h3>
       </div>
     )
   }
--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { fetchCollection } from '../lib/collection'
 
 import './Collection.css'
-import { Card } from './Card'
+import { Card } from '../components/Card'
 
 export const Collection = () => {
   const collection = fetchCollection()
